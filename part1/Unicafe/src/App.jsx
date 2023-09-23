05c9eba4795fb85279f4450351071167368dbf38
Mathieu Furlan <mathieu.furlan@inseec-france.com>
2023-09-23 19:26:40 +0200

    Modified file ex 1.7

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -8,19 +8,28 @@ const App = () => {
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
+      <button onClick={() => {
+        setGood(good + 1)
+        }}>
         good
       </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
+      <button onClick={() => {
+        setNeutral(neutral + 1)
+        }}>
         neutral
       </button>
-      <button onClick={() => setBad(bad + 1)}>
+      <button onClick={() => {
+        setBad(bad + 1)
+        }}>
         bad
       </button>
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100}%</p>
     </div>
   )
 }

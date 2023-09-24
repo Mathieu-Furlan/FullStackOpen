41573b18063b7ae400cf1fc2854d2d1ad451bb95
Mathieu Furlan <mathieu.furlan@inseec-france.com>
2023-09-24 13:06:58 +0200

    Modified file according ex 1.8

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  return (
+    <div>
+      {props.text}{props.stat}
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -24,12 +32,12 @@ const App = () => {
         bad
       </button>
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {(good / (good + neutral + bad)) * 100}%</p>
+      <Statistics text='good ' stat={good} />
+      <Statistics text='neutral ' stat={neutral} />
+      <Statistics text='bad ' stat={bad} />
+      <Statistics text='all ' stat={good + neutral + bad} />
+      <Statistics text='average ' stat={(good - bad) / (good + neutral + bad)} />
+      <Statistics text='positive ' stat={((good / (good + neutral + bad)) * 100)+ "%"} />
     </div>
   )
 }

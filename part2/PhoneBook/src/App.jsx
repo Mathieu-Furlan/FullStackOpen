c398296318ada0510e5cc4c1a84f9898cc682cc6
Mathieu Furlan <mathieu.furlan@inseec-france.com>
2023-09-28 18:39:29 +0200

    Exercise 2.9

--- part2/PhoneBook/src/App.jsx
+++ part2/PhoneBook/src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchWord, setSearchWord] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addName = (event) => {
     event.preventDefault()
@@ -22,7 +24,16 @@ const App = () => {
     setPersons(persons.concat(nameObj))
     setNewName('')
     setNewNumber('')
+    setSearchWord('')
+    setShowAll(true)
   }
+
+  const showPersons = searchWord === '' || showAll ? persons : persons.filter(person => {
+    if(person.name.toLowerCase().indexOf(searchWord.toLowerCase()) >= 0){
+      return true
+    }
+    return false
+  })
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -30,22 +41,28 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearchChange = (event) => {
+    setSearchWord(event.target.value)
+    setShowAll(false)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with<input value={searchWord} onChange={handleSearchChange} />
+      <h2>add a new</h2>
       <form onSubmit = {addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => 
+        {showPersons.map(person => 
           <div key={person.name}>{person.name} {person.number}</div>
         )}
     </div>

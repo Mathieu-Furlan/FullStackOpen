cf2d0a4441c6e32f6f368c4df6e51c1cbc6d053d
Mathieu Furlan <mathieu.furlan@inseec-france.com>
2023-09-29 18:20:28 +0200

    Exercise 2.11

--- part2/PhoneBook/src/App.jsx
+++ part2/PhoneBook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Formulaire = (props) => {
   return (
@@ -37,13 +38,19 @@ const Filtre = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchWord, setSearchWord] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
